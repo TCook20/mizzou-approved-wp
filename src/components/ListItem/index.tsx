ed957be348c5998c03aacafcdd8e543e960e3bb0
Travis Cook <cooktw@missouri.edu>
2023-09-06 15:13:50 -0400

    Format

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,32 +4,20 @@ import classNames from 'classnames'
 import './style.scss'
 
 export interface ListItemProps {
-  children: React.ReactNode,
-  attributes?: object,
+  children: React.ReactNode
+  attributes?: object
   status: string
 }
 
-const ListItem = ({
-  children,
-  attributes,
-  status,
-}: ListItemProps): JSX.Element => {
-
+const ListItem = ({ children, attributes, status }: ListItemProps): JSX.Element => {
   const ListItemAttributes = {
     ...attributes,
-    className: classNames(
-      'miz-package-list-item', 'miz-card',
-      {
-        [`miz-package-list-item--${ status }`]: status
-      }
-    ),
+    className: classNames('miz-package-list-item', 'miz-card', {
+      [`miz-package-list-item--${status}`]: status,
+    }),
   }
 
-  return (
-    <li { ...ListItemAttributes }>
-      { children }
-    </li>
-  )
+  return <li {...ListItemAttributes}>{children}</li>
 }
 
 export default ListItem
